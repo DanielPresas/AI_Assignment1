9e6b53831acc498d571bfb2b7143414719f8af40
Daniel Presas <22991229+SentientCoffee@users.noreply.github.com>
2022-02-10 07:44:28 -0500

    [Add] Build and final tweaks

--- Assets/AI_Assignment1/Scripts/UIManager.cs
+++ Assets/AI_Assignment1/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,6 +11,8 @@ public class UIManager : MonoBehaviour {
 
     public List<string> recentFiles = new List<string>();
 
+    public string customFilepath { get; set; }
+
     private void Start() {
         recentFiles.Insert(0, $"{Application.streamingAssetsPath}/probabilities.txt");
 
@@ -18,18 +21,27 @@ public class UIManager : MonoBehaviour {
             rfButton.transform.SetParent(recentFilePanel);
             rfButton.GetComponentInChildren<TextMeshProUGUI>().text = rf;
             rfButton.GetComponentInChildren<Button>().onClick.AddListener(() => {
-                StartWithRecentFile(rfButton.GetComponentInChildren<TextMeshProUGUI>().text);
+                StartWithFile(rfButton.GetComponentInChildren<TextMeshProUGUI>().text);
             });
         }
     }
 
-    public void StartWithRecentFile(string filepath) {
+    public void StartWithFile(string filepath) {
         if(string.IsNullOrEmpty(filepath)) {
-            Logger.Error("Not a valid filepath!");
+            Logger.Error("Failed to load probabilities because filepath is empty or null!");
+            return;
+        }
+
+        if(!File.Exists(filepath)) {
+            Logger.Error($"Failed to load probabilities because filepath doesn't exist!\nPath: {filepath}");
             return;
         }
 
         GameManager.probabilitiesFilepath = filepath;
         SceneManager.LoadScene(1);
+    }
+
+    public void StartWithCustomFilepath() {
+        StartWithFile(customFilepath);
     }
 }
