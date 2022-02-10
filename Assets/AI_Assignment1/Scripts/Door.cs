66cc338ed407cc37c7d5795a5c7f13832a7a9329
Daniel Presas <22991229+SentientCoffee@users.noreply.github.com>
2022-02-10 03:55:08 -0500

    [Add] Door hot and noisy logic

--- Assets/AI_Assignment1/Scripts/Door.cs
+++ Assets/AI_Assignment1/Scripts/Door.cs
@@ -2,20 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct DoorProbabilities {
+public class DoorProbabilities {
     // yyy => hot = y, noisy = y, safe = y
-    public float yyy, yyn, yny, ynn;
-    public float nyy, nyn, nny, nnn;
+    public enum Key {
+        YYY = 0, YYN, YNY, YNN,
+        NYY, NYN, NNY, NNN,
+    }
+
+    public float[] dict = new float[8];
+
+    public float yyy { get => dict[(int)Key.YYY]; set => dict[(int)Key.YYY] = value; }
+    public float yyn { get => dict[(int)Key.YYN]; set => dict[(int)Key.YYN] = value; }
+    public float yny { get => dict[(int)Key.YNY]; set => dict[(int)Key.YNY] = value; }
+    public float ynn { get => dict[(int)Key.YNN]; set => dict[(int)Key.YNN] = value; }
+    public float nyy { get => dict[(int)Key.NYY]; set => dict[(int)Key.NYY] = value; }
+    public float nyn { get => dict[(int)Key.NYN]; set => dict[(int)Key.NYN] = value; }
+    public float nny { get => dict[(int)Key.NNY]; set => dict[(int)Key.NNY] = value; }
+    public float nnn { get => dict[(int)Key.NNN]; set => dict[(int)Key.NNN] = value; }
 }
 
 public class Door : MonoBehaviour {
-    public bool hot = false;
-    public bool noisy = false;
-    public bool safe = false;
+    public enum Flags {
+        Hot   = 0x1,
+        Noisy = 0x2,
+        Safe  = 0x4,
+    }
 
-    [Space]
-    public Material doorMaterial = null;
+    public bool hot   { get => (doorFlags & (int)Flags.Hot)   > 0; set => doorFlags |= (int)Flags.Hot   * (value ? 1 : 0); }
+    public bool noisy { get => (doorFlags & (int)Flags.Noisy) > 0; set => doorFlags |= (int)Flags.Noisy * (value ? 1 : 0); }
+    public bool safe  { get => (doorFlags & (int)Flags.Safe)  > 0; set => doorFlags |= (int)Flags.Safe  * (value ? 1 : 0); }
+
+    public int doorFlags = 0;
+
+    [Header("Elements to use")]
+    public Material normalMaterial = null;
+    public Material hotMaterial = null;
     public AudioClip noise = null;
 
+    [Header("Object references")]
+    public new Renderer renderer = null;
+    public AudioSource audioSource = null;
+
+    private void Start() {
+        audioSource.enabled = false;
+        renderer.material = normalMaterial;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        audioSource.PlayOneShot(noise);
+    }
+
+    public void ResetBasedOnFlags() {
+        Logger.Log($"{gameObject.name} flags: {doorFlags:x} (hot = {hot}, noisy = {noisy}, safe = {safe})");
+
+        renderer.material = hot ? hotMaterial : normalMaterial;
+        audioSource.enabled = noisy;
+    }
 
 }
--- Assets/AI_Assignment1/Scripts/GameManager.cs
+++ Assets/AI_Assignment1/Scripts/GameManager.cs
@@ -3,19 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public bool debug = false;
+
+    public GameObject player = null;
+
     public List<Door> doors = new List<Door>();
+    public Transform spawnPoint;
+
+    public static DoorProbabilities probabilities;
 
     private void Start() {
         // @Todo: Find "probabilities.txt" in other places
         var ret = FileManager.LoadFile(Application.streamingAssetsPath + "/probabilities.txt");
-        if (!ret.success) {
+        if(!ret.success) {
             Logger.Log($"Failed to load {Application.streamingAssetsPath + "/probabilities.txt"}");
+            return;
         }
 
+        probabilities = ret.probabilities;
+        Random.InitState((int)System.DateTime.Now.ToFileTimeUtc());
 
+        // for(int i = 0; i < probabilities.dict.Length; i++) {
+        //     float p = probabilities.dict[i];
+        //     Logger.Log($"{(DoorProbabilities.Key)i}: {p}");
+        // }
+    }
+
+    private void Update() {
+        if(debug) {
+            RandomizeDoors();
+            debug = false;
+        }
     }
 
     public void RandomizeDoors() {
+        DoorProbabilities.Key GetRandomType() {
+            var rand = Random.Range(0.0f, 1.0f);
+            // Logger.Log($"rand: {rand}");
+
+            var chosenType = DoorProbabilities.Key.YYY;
+            for(int i = 0; i < probabilities.dict.Length; i++) {
+                float p = probabilities.dict[i];
+                if(rand <= p) {
+                    chosenType = (DoorProbabilities.Key)i;
+                    break;
+                }
+                rand -= p;
+            }
+
+            // Logger.Log($"chosenType: {chosenType}");
+            return chosenType;
+        }
+
+        foreach(var d in doors) {
+            var type = GetRandomType();
+            switch(type) {
+                case DoorProbabilities.Key.YYY: d.doorFlags |= (int)Door.Flags.Hot | (int)Door.Flags.Noisy | (int)Door.Flags.Safe; break;
+                case DoorProbabilities.Key.YYN: d.doorFlags |= (int)Door.Flags.Hot | (int)Door.Flags.Noisy;                        break;
+                case DoorProbabilities.Key.YNY: d.doorFlags |= (int)Door.Flags.Hot                         | (int)Door.Flags.Safe; break;
+                case DoorProbabilities.Key.YNN: d.doorFlags |= (int)Door.Flags.Hot;                                                break;
+                case DoorProbabilities.Key.NYY: d.doorFlags |=                       (int)Door.Flags.Noisy | (int)Door.Flags.Safe; break;
+                case DoorProbabilities.Key.NYN: d.doorFlags |=                       (int)Door.Flags.Noisy;                        break;
+                case DoorProbabilities.Key.NNY: d.doorFlags |=                                               (int)Door.Flags.Safe; break;
+                case DoorProbabilities.Key.NNN: d.doorFlags  = 0;                                                                  break;
+            }
+
+            d.ResetBasedOnFlags();
+        }
 
     }
 }
